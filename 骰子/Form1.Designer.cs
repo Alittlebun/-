bfd8b94638f49e1e20e315b1920090df17e741bd
Alittlebun <ice1541321b520@gmail.com>
2020-11-19 20:15:34 +0800

    加入專案檔案。

---
+++ 骰子/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace 骰子
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGO = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGO
+            // 
+            this.btnGO.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnGO.Font = new System.Drawing.Font("新細明體", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGO.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnGO.Location = new System.Drawing.Point(239, 160);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(247, 158);
+            this.btnGO.TabIndex = 0;
+            this.btnGO.Text = "GO";
+            this.btnGO.UseVisualStyleBackColor = false;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGO;
+    }
+}
+
---
+++ 骰子/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 骰子
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            int randomNum = new Random().Next(100)%6+1; //0-100
+            MessageBox.Show($"{randomNum}","Random Num",MessageBoxButtons.OK);
+        }
+    }
+}
